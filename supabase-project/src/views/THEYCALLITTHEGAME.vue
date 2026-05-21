be697821311553eb5f12a3661c0d7566187ca451
aidenl93 <aidenl93@nycstudents.net>
2026-05-21 13:49:11 -0400

    to the main

---
+++ supabase-project/src/views/THEYCALLITTHEGAME.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+let counter = 10 
+let y = 0;
+let x = 1;
+setInterval (() => {
+    counter -= 1
+    console.log(counter)
+}, 1000)
+function theGame() {
+const timer = setInterval(() => {
+  y = Math.random() * 10;
+  console.log(x);
+  if (y <= 9.495) {
+    console.log("yesss");
+    x += 0.05;
+  } else {
+    console.log("no");
+    clearInterval(timer);
+  }
+}, 100);}
+
+</script>
+
+<style>
+
+</style>
