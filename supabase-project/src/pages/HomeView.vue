e80f77351941439fd543c8c9803463162b91c565
Juiceman78 <vincentz78@nycstudents.net>
2026-05-22 14:01:04 -0400

    Raymond method

--- supabase-project/src/pages/HomeView.vue
+++ supabase-project/src/pages/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <h1>HI</h1>
 </template>
 
 <script setup></script>
--- supabase-project/src/pages/LoginView.vue
+++ supabase-project/src/pages/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <input type="text" />
 </template>
 
 <script setup></script>
