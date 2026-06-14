f9b400743a43b3c55ef3149e4b72375d40eab4c7
Juiceman78 <vincentz78@nycstudents.net>
2026-06-14 19:01:49 -0400

    a
    okay

--- nuxt-app/.nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('C:/Users/aiden.liu24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('C:/Users/aiden.liu24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('C:/Users/aiden.liu24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('C:/Users/aiden.liu24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}
