9dca32acc77c94fa671c7cc09550670c3d0e502a
Juiceman78 <vincentz78@nycstudents.net>
2026-06-15 11:38:38 -0400

    okay please work netlify

--- nuxt-app/.nuxt/nuxt.node.d.ts
+++ nuxt-app/.nuxt/nuxt.node.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="@pinia/nuxt" />
-/// <reference types="@nuxt/telemetry" />
 /// <reference types="@nuxt/devtools" />
+/// <reference types="@nuxt/telemetry" />
 /// <reference types="@nuxtjs/supabase" />
 /// <reference path="types/nitro-layouts.d.ts" />
 /// <reference path="types/modules.d.ts" />
--- nuxt-app/.nuxt/types/supabase.d.ts
+++ nuxt-app/.nuxt/types/supabase.d.ts
@@ -1,6 +1,6 @@
 declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('C:/Users/godis/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('C:/Users/godis/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('C:/Users/godis/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('C:/Users/godis/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
+  const serverSupabaseClient: typeof import('C:/Users/vincent.zhen24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
+  const serverSupabaseServiceRole: typeof import('C:/Users/vincent.zhen24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
+  const serverSupabaseUser: typeof import('C:/Users/vincent.zhen24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
+  const serverSupabaseSession: typeof import('C:/Users/vincent.zhen24/Documents/GitHub/vue-supabase-project-2026-clockwise/nuxt-app/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
 }
