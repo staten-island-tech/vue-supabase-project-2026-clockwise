1b695382738b794a3d90d620d4fbcf43d18e6e3c
aidenl93 <aidenl93@nycstudents.net>
2026-06-15 20:49:53 -0400

    sure wtv bro

--- nuxt-app/.nuxt/types/build.d.ts
+++ nuxt-app/.nuxt/types/build.d.ts
@@ -13,7 +13,7 @@ declare module "#build/root-component.mjs";
 declare module "#build/plugins.server.mjs";
 declare module "#build/test-component-wrapper.mjs";
 declare module "#build/devtools/settings.mjs";
-declare module "#build/runtime.vue-devtools-client.gbv3FI_YG_3gX6EiOYgGr54NhOBTOxO9ozEia0TXP5Y.js";
+declare module "#build/runtime.vue-devtools-client.eDaFVqwNGIygoqsmJbjv74JYjfCo1JnYCC_tt_tat20.js";
 declare module "#build/routes.mjs";
 declare module "#build/pages.mjs";
 declare module "#build/router.options.mjs";
